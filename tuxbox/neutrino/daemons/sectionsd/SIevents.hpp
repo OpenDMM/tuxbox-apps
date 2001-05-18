18ab1d579295b74e6b2381590214ce6e74b0083d
fnbrd <fnbrd>
2001-05-18 20:31:04 +0000

    Aenderungen fuer -Wall

--- tuxbox/neutrino/daemons/sectionsd/SIevents.hpp
+++ tuxbox/neutrino/daemons/sectionsd/SIevents.hpp
@@ -24,6 +24,9 @@
 //    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 // $Log$
+// Revision 1.4  2001/05/18 20:31:04  fnbrd
+// Aenderungen fuer -Wall
+//
 // Revision 1.3  2001/05/18 13:11:46  fnbrd
 // Fast komplett, fehlt nur noch die Auswertung der time-shifted events
 // (Startzeit und Dauer der Cinedoms).
@@ -155,13 +158,13 @@ class SIevent {
         printf("Extended-Text: %s\n", extendedText.c_str());
       if(contentClassification.length()) {
         printf("Content classification:");
-        for(int i=0; i<contentClassification.length(); i++)
+        for(unsigned i=0; i<contentClassification.length(); i++)
           printf(" 0x%02hhx", contentClassification[i]);
         printf("\n");
       }
       if(userClassification.length()) {
         printf("User classification:");
-        for(int i=0; i<userClassification.length(); i++)
+        for(unsigned i=0; i<userClassification.length(); i++)
           printf(" 0x%02hhx", userClassification[i]);
         printf("\n");
       }
