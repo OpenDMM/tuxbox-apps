b7fbf029311907751e820051a0a1f82578f02c83
Peter Feldbaumer <field@cvs.tuxbox.org>
2001-11-05 17:13:26 +0000

    wiederholungen...?

--- tuxbox/neutrino/src/gui/infoviewer.cpp
+++ tuxbox/neutrino/src/gui/infoviewer.cpp
@@ -2,6 +2,9 @@
 // $Id$
 //
 // $Log$
+// Revision 1.46  2001/11/05 17:13:26  field
+// wiederholungen...?
+//
 // Revision 1.45  2001/11/05 16:04:25  field
 // nvods/subchannels ver"c++"ed
 //
@@ -589,7 +592,7 @@ void * CInfoViewer::InfoViewerThread (void *arg)
 
                     gettimeofday(&now, NULL);
                     TIMEVAL_TO_TIMESPEC(&now, &abs_wait);
-                    abs_wait.tv_sec += 1;
+                    abs_wait.tv_sec += 2;
 
                     pthread_mutex_trylock( &InfoViewer->epg_mutex );
                     pthread_cond_timedwait( &InfoViewer->epg_cond, &InfoViewer->epg_mutex, &abs_wait );
