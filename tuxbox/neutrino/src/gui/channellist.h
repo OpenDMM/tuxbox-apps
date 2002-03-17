8af8cd44b2c7fe9c7eb52ecac3f516d3a63aae83
Rainer Scherg <rasc@cvs.tuxbox.org>
2002-03-17 19:59:08 +0000

    -- channelselector, info details: --    visible "connector line" between channel  and detail info at bottom

--- tuxbox/neutrino/src/gui/channellist.h
+++ tuxbox/neutrino/src/gui/channellist.h
@@ -35,6 +35,10 @@
 // $Id$
 //
 // $Log$
+// Revision 1.29  2002/03/17 19:59:08  rasc
+// -- channelselector, info details:
+// --    visible "connector line" between channel  and detail info at bottom
+//
 // Revision 1.28  2002/03/11 17:25:57  Simplex
 // locked bouquets work
 //
@@ -190,6 +194,8 @@ class CChannelList
 		int 			y;
 
 		void paintDetails(int index);
+		void clearItem2DetailsLine ();
+		void paintItem2DetailsLine (int pos, int ch_index);
 		void paintItem(int pos);
 		void paint();
 		void paintHead();
