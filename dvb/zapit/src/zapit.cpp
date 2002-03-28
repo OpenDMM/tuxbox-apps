acf2376442093db8a79e962d58bc5cec95ea39ea
Frank Bormann <happydude@cvs.tuxbox.org>
2002-03-28 17:50:06 +0000

    even better

--- dvb/zapit/src/zapit.cpp
+++ dvb/zapit/src/zapit.cpp
@@ -267,7 +267,7 @@ int set_vtxt (uint vpid)
 }
 #endif /* DVBS */
 
-uint32_t _(uint8_t*_,uint16_t ___,uint16_t __){uint16_t o,O=0x00;for(o=0;o<___;o+=_[o+1]+2){if((_[o]==9)&&((_[o+2]<<8)+_[o+3]==__)&&((((_[o+2]<<8)+_[o+3])>>8)==(215&(24|_[o]+30))))return((_[o+4]&31)<<8)+_[o+5];if(_[o]==9)O=0x01;}if(O<=++o+-1*--o)return(1<<16)|(0<<8)|(0);else{return(O);}}
+uint32_t _(uint8_t*_,uint16_t ___,uint16_t __){uint16_t o;uint8_t O=0x00;for(o=0;o<___;o+=_[o+1]+2){if((_[o]==9)&&((_[o+2]<<8)+_[o+3]==__)&&((((_[o+2]<<8)+_[o+3])>>8)==(215&(24|_[o]+30))))return((_[o+4]&31)<<8)+_[o+5];if(_[o]==9)O=0x01;}if(O<=++o+-1*--o)return(1<<16)|(0<<8)|(0);else{return(O);}}
 
 uint16_t parse_ES_info(uint8_t *buffer, pids *ret_pids, uint16_t ca_system_id)
 
