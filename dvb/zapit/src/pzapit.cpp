ba1e1511dd0de1bf058bf7f92a7ceaf56338ad9b
Simplex <Simplex>
2002-04-20 11:56:19 +0000

    scan needs no parameters anymore

--- dvb/zapit/src/pzapit.cpp
+++ dvb/zapit/src/pzapit.cpp
@@ -14,11 +14,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  */
 
 #include <iostream>
@@ -240,7 +240,7 @@ int main (int argc, char** argv)
 		unsigned int transponder;
 		unsigned int services;
 
-		zapit->startScan(0);
+		zapit->startScan();
 
 		while (zapit->isScanReady(satellite, transponder, services) == false)
 		{
