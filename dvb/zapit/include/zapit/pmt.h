a95d54a9191f9782ae939c45bd2aee51799c626c
Peter Feldbaumer <field@cvs.tuxbox.org>
2002-04-24 18:51:18 +0000

    PMT/PAT-parsing intelligenter gestaltet :)

--- dvb/zapit/include/zapit/pmt.h
+++ dvb/zapit/include/zapit/pmt.h
@@ -10,6 +10,7 @@
 #include "getservices.h"
 
 uint16_t parse_ES_info(uint8_t *buffer, pids *ret_pids, uint16_t ca_system_id);
-pids parse_pmt (dvb_pid_t pmt_pid, uint16_t ca_system_id, uint16_t program_number);
+int init_parse_pmt(dvb_pid_t pmt_pid, uint16_t program_number);
+pids parse_pmt (dvb_pid_t pmt_pid, uint16_t ca_system_id, int fd);
 
 #endif /* __pmt_h__ */
