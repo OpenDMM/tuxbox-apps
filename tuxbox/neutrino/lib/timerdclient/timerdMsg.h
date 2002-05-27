9edeb41d17cc2044b80fb904713c122d5118002c
Dirk Szymanski <dirch@cvs.tuxbox.org>
2002-05-27 21:53:30 +0000

    externalCommand changed

--- tuxbox/neutrino/lib/timerdclient/timerdMsg.h
+++ tuxbox/neutrino/lib/timerdclient/timerdMsg.h
@@ -51,7 +51,7 @@ class CTimerd
 
 		static const char ACTVERSION = 1;
 
-		enum externalcommand 
+		enum externalcommands 
 		{
 			CMD_VCR_UNKNOWN =	0;
 			CMD_VCR_START	=	1;
@@ -63,10 +63,11 @@ class CTimerd
 
 		struct externalCommand
 		{
-			unsigned char messageType;		// maybe vcr or server ?
-			unsigned char version;			// ACTVERSION
-			unsigned int command;			// externalcommand
-			unsigned long long epgID;		// may be zero
+			unsigned char		messageType;		// maybe vcr or server ?
+			unsigned char		version;			// ACTVERSION
+			unsigned int		command;			// externalcommands
+			unsigned long long	epgID;				// may be zero
+			unsigned int		onidsid;			// may be zero
 		};
 
 		enum commands
