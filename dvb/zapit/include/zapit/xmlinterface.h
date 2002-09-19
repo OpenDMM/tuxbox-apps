a8d33d388d390e72ac6f232ac72aa59808cd5746
Andreas Oberritter <obi@cvs.tuxbox.org>
2002-09-19 12:10:26 +0000

    use apps/libxmltree

--- dvb/zapit/include/zapit/xmlinterface.h
+++ dvb/zapit/include/zapit/xmlinterface.h
@@ -21,12 +21,16 @@
  *
  */
 
+#ifndef __xmlinterface_h__
+#define __xmlinterface_h__
+
 #include <string>
 
-#include "xml/xmltree.h"
-
+#include <xmltree.h>
 
 std::string convertForXML(const std::string s);
 std::string Utf8_to_Latin1(const std::string s);
 
 XMLTreeParser* parseXmlFile(const std::string filename);
+
+#endif /* __xmlinterface_h__ */
