a42d5b56d00ab5baf023bfd564600927f718e2ac
Ralf Gandy <thegoodguy@cvs.tuxbox.org>
2002-10-03 19:31:03 +0000

    Renumbering of bouquets (Part IV) - Final

--- tuxbox/neutrino/daemons/nhttpd/bouqueteditapi.cpp
+++ tuxbox/neutrino/daemons/nhttpd/bouqueteditapi.cpp
@@ -99,7 +99,7 @@ bool CBouqueteditAPI::Execute(CWebserverRequest* request)
 		}
 		else
 		{
-			if (Parent->Zapit->existsBouquet(request->ParameterList["name"]) == 0) {
+			if (Parent->Zapit->existsBouquet(request->ParameterList["name"]) == -1) {
 				Parent->Zapit->addBouquet(request->ParameterList["name"]);
 				request->Send302("/bouquetedit/main#akt");
 			} else {
@@ -244,7 +244,7 @@ bool CBouqueteditAPI::Execute(CWebserverRequest* request)
 			request->SocketWrite("<select multiple size=\"20\" name=\"achannels\">\n");
 			for(; allChannels != AllChannelList.end();allChannels++)
 			{
-				if (!Parent->Zapit->existsChannelInBouquet(selected, allChannels->channel_id)){
+				if (!Parent->Zapit->existsChannelInBouquet(selected - 1, allChannels->channel_id)){
 					sprintf(outbuff, "<option value=\"%i\">", allChannels->channel_id);
 					request->SocketWrite(outbuff);
 					request->SocketWrite(allChannels->name);
