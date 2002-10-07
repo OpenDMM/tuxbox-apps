ddb4c92fc9240f679b5d49474f03159ec540a3c4
Ralf Gandy <thegoodguy@cvs.tuxbox.org>
2002-10-07 14:46:41 +0000

    Use UTF-8 Encoding in "bouquets.xml" & autodetect encoding for older "bouquets.xml"s

--- dvb/zapit/include/zapit/xmlinterface.h
+++ dvb/zapit/include/zapit/xmlinterface.h
@@ -29,6 +29,7 @@
 #include <xmltree.h>
 
 std::string convertForXML(const std::string s);
+std::string convert_to_UTF8_XML(const std::string s);
 std::string Utf8_to_Latin1(const std::string s);
 
 XMLTreeParser* parseXmlFile(const std::string filename);
