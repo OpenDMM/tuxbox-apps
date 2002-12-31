fe3f47b0575d257845aeabfc78d7560ee0b02d52
Ghostrider <Ghostrider>
2002-12-31 14:37:20 +0000

    fix lcd bug, add harddisc check.. not finished yet... code is not nice...

--- tuxbox/enigma/src/enigma_info.cpp
+++ tuxbox/enigma/src/enigma_info.cpp
@@ -42,7 +42,7 @@ eZapInfo::eZapInfo()
 	move(ePoint(150, 136));
 	CONNECT((new eListBoxEntryMenu(&list, _("[back]"), _("go back to mainmenu")))->selected, eZapInfo::sel_close);
 	CONNECT((new eListBoxEntryMenu(&list, _("Streaminfo"), _("open the Streaminfo")))->selected, eZapInfo::sel_streaminfo);
-	if ( eDVB::getInstance()->getInfo("mID") != "05" && eDVB::getInstance()->getInfo("mID") != "06" )
+	if ( atoi( eDVB::getInstance()->getInfo("mID").c_str() ) > 4 )
 		CONNECT((new eListBoxEntryMenu(&list, _("Show BN version"),_("show the Current Version of the Betanova FW")))->selected, eZapInfo::sel_bnversion);
 
 	CONNECT((new eListBoxEntryMenu(&list, _("About..."), _("open the about dialog")))->selected, eZapInfo::sel_about);
