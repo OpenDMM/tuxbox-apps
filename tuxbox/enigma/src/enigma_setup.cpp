b4fa1f54f815dc235a0eb4971333213645e11cff
Bernhard Roth <bernroth@cvs.tuxbox.org>
2003-02-28 21:55:06 +0000

    extra setup help in eZapMain added

--- tuxbox/enigma/src/enigma_setup.cpp
+++ tuxbox/enigma/src/enigma_setup.cpp
@@ -32,6 +32,7 @@
 #include <setup_rc.h>
 #include <setup_harddisk.h>
 #include <setup_rfmod.h>
+#include <setup_extra.h>
 #include <enigma_ci.h>
 #include <enigma_scan.h>
 #include <setupskin.h>
@@ -99,6 +100,7 @@ eZapSetup::eZapSetup()
 	CONNECT((new eListBoxEntryMenu(&list, _("Skin..."), eString().sprintf("(%d) %s", ++entry, _("open skin selector")) ))->selected, eZapSetup::sel_skin);
 	CONNECT((new eListBoxEntryMenu(&list, _("Language..."), eString().sprintf("(%d) %s", ++entry, _("open language selector")) ))->selected, eZapSetup::sel_language);
 	CONNECT((new eListBoxEntryMenu(&list, _("Ngrab..."), eString().sprintf("(%d) %s", ++entry, _("open ngrab config")) ))->selected, eZapSetup::sel_engrab);
+	CONNECT((new eListBoxEntryMenu(&list, _("Extra settings..."), eString().sprintf("(%d) %s", ++entry, _("open extra config")) ))->selected, eZapSetup::sel_extra);
 	if (haveharddisk)
 		CONNECT((new eListBoxEntryMenu(&list, _("Harddisk..."), eString().sprintf("(%d) %s", ++entry, _("open harddisk setup")) ))->selected, eZapSetup::sel_harddisk);
 	if (haveci)
@@ -213,10 +215,22 @@ void eZapSetup::sel_video()
 	setup.hide();
 	show();
 }
+
 void eZapSetup::sel_engrab()
 {
 	hide();
 	ENgrabSetup setup;
+	setup.setLCD(LCDTitle, LCDElement);
+	setup.show();
+	setup.exec();
+	setup.hide();
+	show();
+}
+
+void eZapSetup::sel_extra()
+{
+	hide();
+	eZapExtraSetup setup;
 	setup.setLCD(LCDTitle, LCDElement);
 	setup.show();
 	setup.exec();
