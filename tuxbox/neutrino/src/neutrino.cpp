dbcdd9db1005637340d84f74daaa18d721cb1f7b
Alexander Wild <alexw@cvs.tuxbox.org>
2003-04-15 18:33:50 +0000

    ucode.bin buildin support

--- tuxbox/neutrino/src/neutrino.cpp
+++ tuxbox/neutrino/src/neutrino.cpp
@@ -767,10 +767,11 @@ void CNeutrinoApp::doChecks()
 	if(fd)
 		fclose(fd);
 	ucodes_ok= ucodes_ok||(fd);
+/*	ucode.bin no longer needed, since buildin
 	fd = fopen(UCODEDIR "/ucode.bin", "r");
 	if(fd)
 		fclose(fd);
-	ucodes_ok= ucodes_ok&&(fd);
+	ucodes_ok= ucodes_ok&&(fd);*/
 	fd = fopen(UCODEDIR "/cam-alpha.bin", "r");
 	if(fd)
 		fclose(fd);
