4ea5cc893e4757000c7595b9b6ea31a2c293a628
Ronny Strutz <tripledes@cvs.tuxbox.org>
2003-05-10 00:12:59 +0000

    some changes / fixes

--- tuxbox/enigma/src/enigma_scan.cpp
+++ tuxbox/enigma/src/enigma_scan.cpp
@@ -46,7 +46,9 @@ eZapScan::eZapScan()
 		CONNECT((new eListBoxEntryMenu(&list, _("Satellites..."), _("goto satellite config")))->selected, eZapScan::sel_satconfig);
 		CONNECT((new eListBoxEntryMenu(&list, _("Motor Setup..."), _("goto Motor Setup")))->selected, eZapScan::sel_rotorConfig);
 	}
-	CONNECT((new eListBoxEntryMenu(&list, _("SID...")))->selected, eZapScan::sel_bouquet);
+	
+	if(eDVB::getInstance()->getmID() != 6)
+		CONNECT((new eListBoxEntryMenu(&list, _("SID...")))->selected, eZapScan::sel_bouquet);
 }
 
 eZapScan::~eZapScan()
