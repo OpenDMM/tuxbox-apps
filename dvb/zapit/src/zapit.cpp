394ff6b3d5ada1e9480bda07d6696391303a2dce
Peter Reich <digi_casi@cvs.tuxbox.org>
2003-05-11 15:23:04 +0000

    save current satellite position in zapit.conf

--- dvb/zapit/src/zapit.cpp
+++ dvb/zapit/src/zapit.cpp
@@ -125,6 +125,7 @@ void saveSettings(bool write)
 	}
 
 	if (write) {
+		config.setInt32("lastSatellitePosition", frontend->getCurrentSatellitePosition());
 		config.setInt32("diseqcRepeats", frontend->getDiseqcRepeats());
 		config.setInt32("diseqcType", frontend->getDiseqcType());
 		config.saveConfig(CONFIGFILE);
@@ -1367,6 +1368,7 @@ void leaveStandby(void)
 		videoDecoder = new CVideo();
 	}
 
+	frontend->setCurrentSatellitePosition(config.getInt32("lastSatellitePosition", 192));
 	frontend->setDiseqcType((diseqc_t) config.getInt32("diseqcType", NO_DISEQC));
 	frontend->setDiseqcRepeats(config.getInt32("diseqcRepeats", 0));
 
