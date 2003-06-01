464ffcd6531722638c8be747111e2ed3403574c4
Peter Reich <digi_casi@cvs.tuxbox.org>
2003-06-01 17:23:18 +0000

    make satellitePosition 16 bit

--- dvb/zapit/include/zapit/getservices.h
+++ dvb/zapit/include/zapit/getservices.h
@@ -37,11 +37,11 @@
 #define NONE 0x0000
 #define INVALID 0x1FFF
 
-void ParseTransponders(xmlNodePtr xmltransponder, const unsigned char DiSEqC, std::string satellite, int32_t satellitePosition);
-void ParseChannels    (xmlNodePtr node, const t_transport_stream_id transport_stream_id, const t_original_network_id original_network_id, const unsigned char DiSEqC, std::string satellite, int32_t satellitePosition);
+void ParseTransponders(xmlNodePtr xmltransponder, const unsigned char DiSEqC, std::string satellite, t_satellite_position satellitePosition);
+void ParseChannels    (xmlNodePtr node, const t_transport_stream_id transport_stream_id, const t_original_network_id original_network_id, const unsigned char DiSEqC, std::string satellite, t_satellite_position satellitePosition);
 void FindTransponder  (xmlNodePtr root);
 void LoadSortList     ();
-int LoadServices      (uint32_t);
+int LoadServices      (diseqc_t);
 
 struct transponder
 {
