21fc26b4e9ca65534b09f4dce77464db860be99a
Peter Reich <digi_casi@cvs.tuxbox.org>
2003-06-04 16:17:01 +0000

    type of satellite position from uint to int

--- dvb/zapit/include/zapit/client/zapittypes.h
+++ dvb/zapit/include/zapit/client/zapittypes.h
@@ -37,8 +37,8 @@ typedef uint16_t t_original_network_id;
 typedef uint16_t t_transport_stream_id;
 #define SCANF_TRANSPORT_STREAM_ID_TYPE "%hx"
 
-typedef uint16_t t_satellite_position;
-#define SCANF_SATELLITE_POSITION_TYPE "%hd"
+typedef int16_t t_satellite_position;
+#define SCANF_SATELLITE_POSITION_TYPE "%d"
 
 typedef uint16_t t_network_id;
 
