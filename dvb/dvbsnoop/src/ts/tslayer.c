5f444666f76d831cd4fe92485b5bebf7c3db20fc
J.M. Degen <coronas@cvs.tuxbox.org>
2003-10-28 19:33:08 +0000

    Compilefix rel-branch/Update from HEAD

--- dvb/dvbsnoop/src/ts/tslayer.c
+++ dvb/dvbsnoop/src/ts/tslayer.c
@@ -7,6 +7,12 @@ $Id$
 
 
 $Log$
+Revision 1.3.2.1  2003/10/28 19:33:24  coronas
+Compilefix rel-branch/Update from HEAD
+
+Revision 1.4  2003/10/24 22:17:24  rasc
+code reorg...
+
 Revision 1.3  2002/08/17 20:36:12  obi
 no more compiler warnings
 
@@ -24,7 +30,9 @@ dvbsnoop v0.7  -- Commit to CVS
 
 #include "dvbsnoop.h"
 #include "tslayer.h"
-#include "hexprint.h"
+#include "strings/dvb_str.h"
+#include "misc/output.h"
+#include "misc/hexprint.h"
 
 
 
