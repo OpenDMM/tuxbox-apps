6bb1b6cfc0fb81be4edc60c927988a4a78df3be6
Ralf Gandy <thegoodguy@cvs.tuxbox.org>
2004-06-06 18:01:56 +0000

    fix SelectionWidget in update.cpp, Direct Recording: prevent generation of spurious timer entries, finalize LOCALE_XXX changeNotify changes

--- tuxbox/neutrino/src/system/settings.cpp
+++ tuxbox/neutrino/src/system/settings.cpp
@@ -26,6 +26,26 @@
 
 #include <zapit/settings.h>
 
+
+const int default_timing[TIMING_SETTING_COUNT] =
+{
+	60,
+	60,
+	240,
+	6,
+	60
+};
+
+const neutrino_locale_t timing_setting_name[TIMING_SETTING_COUNT] =
+{
+	LOCALE_TIMING_MENU,
+	LOCALE_TIMING_CHANLIST,
+	LOCALE_TIMING_EPG,
+	LOCALE_TIMING_INFOBAR,
+	LOCALE_TIMING_FILEBROWSER
+};
+
+
 CScanSettings::CScanSettings(void)
 	: configfile('\t')
 {
