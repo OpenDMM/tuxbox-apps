1371058769aaa820ad5b544d3c65a69a06fa192f
Rainer Scherg <rasc@cvs.tuxbox.org>
2005-05-02 15:15:22 +0000

    improve sectionsd/EPG startup. check if box already has a valid time set on startup (e.g. via ntp)

--- tuxbox/neutrino/daemons/sectionsd/sectionsd.cpp
+++ tuxbox/neutrino/daemons/sectionsd/sectionsd.cpp
@@ -91,7 +91,7 @@
 // 12h Pause für SDT
 //#define TIME_SDT_SCHEDULED_PAUSE 12* 60* 60
 // -- shorter time for pause should  result in better behavior  (rasc, 2005-05-02)
-#define TIME_SDT_SCHEDULED_PAUSE 1* 60* 60
+#define TIME_SDT_SCHEDULED_PAUSE 2* 60* 60
 #define TIME_SDT_SKIPPING 5
 
 
@@ -2903,6 +2903,25 @@ static void *timeThread(void *)
 	try
 	{
 		dprintf("[%sThread] pid %d start\n", "time", getpid());
+
+
+		// -- check if time is already on box (e.g. using rdate/ntpd)  (2005-05-02 rasc)
+		// -- if so skip first_time, etc. flags for better/quick EPG startup
+		{
+		  time_t actTime;
+		  struct tm *tmTime;
+		  actTime=time(NULL);
+		  tmTime = localtime(&actTime);
+
+		  // -- do we already have a valid(???) date/time?
+		  if ((tmTime->tm_year + 1900) >= 2005) {
+			first_time = false;
+			timeset = true;
+		  	dprintf("we arlready have a time set\n");
+		  }
+		}
+
+
 
 		while(1)
 		{
