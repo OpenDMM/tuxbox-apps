f53cdb44ba513f7d5e9f6c2d939fea46fc4abbaf
Carsten Juttner <carjay@cvs.tuxbox.org>
2005-05-26 19:20:39 +0000

    email fix

--- tuxbox/tools/misc/audioplay.c
+++ tuxbox/tools/misc/audioplay.c
@@ -3,7 +3,7 @@
  *
  * $Id$
  *
- * Copyright (C) 2004 Carsten Juttner <carjay@gmx.met>
+ * Copyright (C) 2004 Carsten Juttner <carjay@gmx.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
