25071f832577d8c3cc4eed26021c949b9926b514
Peter Reich <digi_casi@cvs.tuxbox.org>
2005-10-22 19:52:34 +0000

    don't show bootmenue if just flash image is available

--- tuxbox/enigma/boot/bootmenue/bootmenue.cpp
+++ tuxbox/enigma/boot/bootmenue/bootmenue.cpp
@@ -43,7 +43,7 @@ stmenu::stmenu()
 	CONNECT(CTimer::getInstance()->selected, stmenu::timeout);
 	config->load();
 	CTimer::getInstance()->start(atoi(config->timeoutValue.c_str()));
-	if (loadImageList() > 0)
+	if (loadImageList() > 1)
 	{
 		loadSkin();
 		showpic();
