7de3760738f69926434c73f4221f7551da20a04f
Peter Reich <digi_casi@cvs.tuxbox.org>
2005-11-13 09:52:58 +0000

    webif: fix movie download function for filenames with umlauts

--- tuxbox/enigma/src/enigma_dyn.h
+++ tuxbox/enigma/src/enigma_dyn.h
@@ -23,9 +23,9 @@
 #define __enigma_dyn_h
 
 #ifdef ENABLE_EXPERT_WEBIF
-#define WEBIFVERSION "5.6.0-Expert"
+#define WEBIFVERSION "5.6.1-Expert"
 #else
-#define WEBIFVERSION "5.6.0"
+#define WEBIFVERSION "5.6.1"
 #endif
 
 #define ZAPMODETV 0
