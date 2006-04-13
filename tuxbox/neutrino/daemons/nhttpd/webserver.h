ca1eb7ee7cf81391487bdd93ff99fb6f8c6a015c
Johannes Golombek <yjogol@cvs.tuxbox.org>
2006-04-13 11:10:44 +0000

    nhttpd 2.2.3 - nhttpd: add Timer: rec_dir from neutrino.conf as default - nhttpd: fixed lost Data in POST-Method (Mozilla/Firefox)

--- tuxbox/neutrino/daemons/nhttpd/webserver.h
+++ tuxbox/neutrino/daemons/nhttpd/webserver.h
@@ -41,7 +41,7 @@
 #define SA	struct sockaddr
 #define SAI	struct sockaddr_in
 
-#define NHTTPD_VERSION "2.2.2"
+#define NHTTPD_VERSION "2.2.3"
 
 #define PRIVATEDOCUMENTROOT	"/share/tuxbox/neutrino/httpd-y"
 #define PUBLICDOCUMENTROOT	"/var/httpd"
