2b07dbedc910061ba480f9e1b8883ff9b41ed7a5
Christian Welzel <feynman@cvs.tuxbox.org>
2007-03-09 21:08:45 +0000

    Neue LCD Statuszeile: Lautstärke, Fortschritt, Audio

--- tuxbox/neutrino/src/driver/lcdd.h
+++ tuxbox/neutrino/src/driver/lcdd.h
@@ -95,7 +95,7 @@ class CLCD
 		LcdFontRenderClass		*fontRenderer;
 		FontsDef			fonts;
 
-#define LCD_NUMBER_OF_BACKGROUNDS 5
+#define LCD_NUMBER_OF_BACKGROUNDS 6
 		raw_display_t                   background[LCD_NUMBER_OF_BACKGROUNDS];
 
 		MODES				mode;
