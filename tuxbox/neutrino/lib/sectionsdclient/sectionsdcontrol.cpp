edb227d16bbbb2b4612320f1d3603651ca184ceb
Harald Kuethe <houdini@cvs.tuxbox.org>
2007-03-17 22:35:09 +0000

    add freemem command

--- tuxbox/neutrino/lib/sectionsdclient/sectionsdcontrol.cpp
+++ tuxbox/neutrino/lib/sectionsdclient/sectionsdcontrol.cpp
@@ -33,6 +33,7 @@ void usage(void) {
 	printf("        sectionsdcontrol --state         get sectionsd runstate\n");
 	printf("        sectionsdcontrol --wepg <epgdir> write epgfiles to dir\n");
 	printf("        sectionsdcontrol --repg <epgdir> read epgfiles from dir\n");
+	printf("        sectionsdcontrol --freemem       unloads all events\n");
 }
 
 int main(int argc, char** argv)
@@ -80,6 +81,11 @@ int main(int argc, char** argv)
 			} else {
 				usage();
 			}
+		}
+		else if (!strcmp(argv[i], "--freemem"))
+		{
+			printf("freeMemory\n");
+			client.freeMemory();
 		}
 	}
 
