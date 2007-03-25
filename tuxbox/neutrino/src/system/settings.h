ee9923aca7b710831619d1f386fe4543c38a6706
ari saastamoinen <Arzka@cvs.tuxbox.org>
2007-03-25 04:33:59 +0000

    Removed "Record PMT pid" in the settings on file recording (It violates DVB specs)
    Replaced it with "Record subtitles"
    
    TODO: Someone german speaker should fix(?) the german.locale I committed.
    I used a text "Subtitle aufzeichnen" and I have no idea what it should be :)
    (The word "Subtitle" should be plural)

--- tuxbox/neutrino/src/system/settings.h
+++ tuxbox/neutrino/src/system/settings.h
@@ -191,7 +191,7 @@ struct SNeutrinoSettings
 	int recording_audio_pids_alt;
 	int recording_audio_pids_ac3;
 	int  recording_stream_vtxt_pid;
-	int  recording_stream_pmt_pid;
+	int  recording_stream_subtitle_pid;
 	char recording_ringbuffers[10];
 	int recording_in_spts_mode;
 	int recording_choose_direct_rec_dir;
