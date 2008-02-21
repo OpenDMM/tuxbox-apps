52035a5cc0313a68d96e5f5e444238469df8a4b9
Frank Liebelt <ecosys@cvs.tuxbox.org>
2008-02-21 18:23:27 +0000

    use better implementation for rounded corners... Example: paintBoxRelSmooth(10, 10, 30, 30, 1, COL_INFOBAR_PLUS_0);

--- tuxbox/neutrino/src/driver/framebuffer.h
+++ tuxbox/neutrino/src/driver/framebuffer.h
@@ -43,7 +43,7 @@
 
 typedef struct fb_var_screeninfo t_fb_var_screeninfo;
 
-/** Ausf�hrung als Singleton */
+/** Ausfuehrung als Singleton */
 class CFrameBuffer
 {
 	private:
@@ -131,6 +131,7 @@ class CFrameBuffer
 			};
 		void paintPixel(int x, int y, const fb_pixel_t col);
 
+		void paintBoxRelSmooth(const int x, const int y, const int dx, const int dy, const int rad, const fb_pixel_t col); // rounded Corners
 		void paintBoxRel(const int x, const int y, const int dx, const int dy, const fb_pixel_t col);
 		inline void paintBox(int xa, int ya, int xb, int yb, const fb_pixel_t col) { paintBoxRel(xa, ya, xb - xa, yb - ya, col); }
 
