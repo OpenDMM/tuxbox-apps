9809b400364bc42df5e3372ba0514068e1151ae6
Stefan Seyfried <seife@cvs.tuxbox.org>
2009-01-01 12:46:01 +0000

    use wake_up() instead of setMode(MODE_TVRADIO) to wake up the LCD

--- tuxbox/neutrino/src/neutrino.cpp
+++ tuxbox/neutrino/src/neutrino.cpp
@@ -7,6 +7,9 @@
 	and some other guys
 	Homepage: http://dbox.cyberphoria.org/
 
+	Copyright (C) 2008 Novell, Inc. Author: Stefan Seyfried
+	Copyright (C) 2007, 2008, 2009 Stefan Seyfried
+
 	Kommentar:
 
 	Diese GUI wurde von Grund auf neu programmiert und sollte nun vom
@@ -2373,7 +2376,7 @@ void CNeutrinoApp::RealRun(CMenuWidget &mainMenu)
 						( msg == NeutrinoMessages::SHOW_INFOBAR ) )
 			{
 			         // turn on LCD display
-				CLCD::getInstance()->setMode(CLCD::MODE_TVRADIO);
+				CLCD::getInstance()->wake_up();
 				// show Infoviewer
 				g_InfoViewer->showTitle(channelList->getActiveChannelNumber(), channelList->getActiveChannelName(), channelList->getActiveSatellitePosition(), channelList->getActiveChannel_ChannelID()); // UTF-8
 			}
@@ -2405,7 +2408,7 @@ void CNeutrinoApp::RealRun(CMenuWidget &mainMenu)
 			else
 			{     // turn on LCD display by kicking it
 				if (msg == CRCInput::RC_home)
-					CLCD::getInstance()->setMode(CLCD::MODE_TVRADIO);
+					CLCD::getInstance()->wake_up();
 				handleMsg(msg, data);
 			}
 
