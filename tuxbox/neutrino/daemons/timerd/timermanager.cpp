1c6013500c67676c5b3758c6db69457efc0618ad
Stefan Seyfried <seife@cvs.tuxbox.org>
2009-01-07 12:11:08 +0000

    fix dreambox compile

--- tuxbox/neutrino/daemons/timerd/timermanager.cpp
+++ tuxbox/neutrino/daemons/timerd/timermanager.cpp
@@ -64,9 +64,11 @@ CTimerManager::CTimerManager()
 	{
 		timer_wakeup = !!wakeup;
 		printf("[timerd] woke up from timer? %s!\n", timer_wakeup ? "true" : "false");
+#ifndef HAVE_DREAMBOX_HARDWARE
 		// clear wakeup event...
 		if (ioctl(fd, FP_IOCTL_CLEAR_WAKEUP_TIMER) < 0)
 			perror("[timerd] FP_IOCTL_CLEAR_WAKEUP_TIMER");
+#endif
 	}
 
 	//thread starten
