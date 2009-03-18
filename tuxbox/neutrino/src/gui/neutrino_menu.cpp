c50af63125d48ed9f5037f920c757b4d7cd331c9
Olaf Martin Gross <rhabarber1848@cvs.tuxbox.org>
2009-03-18 07:15:01 +0000

    next try to fix the mem leak, patch by seife: http://gitorious.org/projects/tuxbox-apps/repos/dm500/commits/5ff3beeb1c13a6b61b282607bf37be8896d551a5

--- tuxbox/neutrino/src/gui/neutrino_menu.cpp
+++ tuxbox/neutrino/src/gui/neutrino_menu.cpp
@@ -1157,7 +1157,7 @@ void CNeutrinoApp::InitLanguageSettings(CMenuWidget &languageSettings)
 		{
 			for(int count=0;count<n;count++)
 			{
-				char * locale = namelist[count]->d_name;
+				char * locale = strdup(namelist[count]->d_name);
 				char * pos = strstr(locale, ".locale");
 				if(pos != NULL)
 				{
@@ -1166,6 +1166,9 @@ void CNeutrinoApp::InitLanguageSettings(CMenuWidget &languageSettings)
 					oj->addOption(locale);
 					languageSettings.addItem( oj );
 				}
+				else
+					free(locale);
+				free(namelist[count]);
 			}
 			free(namelist);
 		}
