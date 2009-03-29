b8e56bd8f5365b911fbf1ecda30ed690058fb9eb
Stefan Seyfried <seife@cvs.tuxbox.org>
2009-03-29 15:58:42 +0000

    audiplayer: fix shadow warning

--- tuxbox/neutrino/src/gui/audioplayer.cpp
+++ tuxbox/neutrino/src/gui/audioplayer.cpp
@@ -362,7 +362,7 @@ int CAudioPlayerGui::show()
 	neutrino_msg_t      msg;
 	neutrino_msg_data_t data;
 
-	int res = -1;
+	int ret = -1;
 
 	CLCD::getInstance()->setMode(CLCD::MODE_AUDIO);
 	paintLCD();
@@ -832,7 +832,7 @@ int CAudioPlayerGui::show()
 	if(m_state != CAudioPlayerGui::STOP)
 		stop();
 
-	return(res);
+	return ret;
 }
 
 bool CAudioPlayerGui::playNext(bool allow_rotate)
