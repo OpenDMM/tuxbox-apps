1f4836168fa569c0ece8183e9d2b2f1da7fa0285
Stefan Seyfried <seife@cvs.tuxbox.org>
2009-04-02 07:53:53 +0000

    stream2file: better debug output

--- tuxbox/neutrino/src/driver/stream2file.cpp
+++ tuxbox/neutrino/src/driver/stream2file.cpp
@@ -195,7 +195,8 @@ void * FileThread(void * v_arg)
 
  retry:
 				sprintf(filename, "%s.%3.3d.%s", myfilename, ++filecount, ((struct filenames_t *)v_arg)->extension);
-				printf("[stream2file] filename: '%s' myfilename: '%s'\n", filename, myfilename);
+				printf("[stream2file] filename: '%s'\n"
+				       "            myfilename: '%s'\n", filename, myfilename);
 				if (fd2 != -1)
 					close(fd2);
 
@@ -502,6 +503,7 @@ stream2file_error_msg_t start_recording(const char * const filename,
 			printf("[stream2file] INFO: %s already exists, not overwriting\n", buf);
 		else
 		{
+			fprintf(stderr, "[stream2file] trying to create %s, error %d (%m)\n", buf, errno);
 			DEC_BUSY_COUNT;
 			return STREAM2FILE_INVALID_DIRECTORY;
 		}
