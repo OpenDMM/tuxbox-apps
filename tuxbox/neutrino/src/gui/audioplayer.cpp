ddfe881bd18a2b2382ecf947dadc22fbf0a1a7b0
Olaf Martin Gross <rhabarber1848@cvs.tuxbox.org>
2009-10-10 20:44:30 +0000

    Neutrino: Increase timeout when reading internetradio playlists: http://tuxbox-forum.dreambox-fan.de/forum/viewtopic.php?f=2&t=48898

--- tuxbox/neutrino/src/gui/audioplayer.cpp
+++ tuxbox/neutrino/src/gui/audioplayer.cpp
@@ -106,7 +106,7 @@
 #define AUDIOPLAYER_END_SCRIPT CONFIGDIR "/audioplayer.end"
 #define DEFAULT_RADIOSTATIONS_XMLFILE CONFIGDIR "/radio-stations.xml"
 
-const long int GET_PLAYLIST_TIMEOUT = 5;
+const long int GET_PLAYLIST_TIMEOUT = 10;
 const char RADIO_STATION_XML_FILE[] = {DEFAULT_RADIOSTATIONS_XMLFILE};
 
 const std::string icecasturl = "http://dir.xiph.org/yp.xml";
