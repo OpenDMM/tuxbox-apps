f61e749863f8505c1a3a7769605bb5a0252dd5a3
Stefan Seyfried <seife@cvs.tuxbox.org>
2010-02-17 11:45:31 +0000

    neutrino: if zapit is in standby mode during start, wake it up
    if neutrino crashed during audio- oder movieplayer, zapit might still
    be in standby during restart, leading to "no channels found", so wake
    it up if necessary

--- tuxbox/neutrino/src/neutrino.cpp
+++ tuxbox/neutrino/src/neutrino.cpp
@@ -1985,6 +1985,12 @@ int CNeutrinoApp::run(int argc, char **argv)
 
 	prepareEnviroment();
 	current_muted = false;
+
+	/* if zapit is in standby, loadSetup() will already get wrong info
+	   (box type etc) */
+	if (g_Zapit->getMode() == CZapitClient::MODE_STANDBY)
+		g_Zapit->setStandby(false);
+
 	int loadSettingsErg = loadSetup();
 	
 	/* load locales before setting up any fonts to determine whether we need a true unicode font */
